a73dc17da0149e769dbb0522f03dec95d88330a7
Firmino Changani <firmino.changani@jungle.ai>
2020-01-13 12:32:16 +0000

    Add a mutation to create tasks

--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -11,5 +11,21 @@ export default {
         return error;
       }
     }
+  },
+  Mutation: {
+    async createTask(parent, args) {
+      try {
+        const newTask = await Models.tasks.create({
+          title: args.title,
+          description: args.description,
+          user_id: args.user_id,
+          status: false
+        });
+
+        return newTask;
+      } catch(error) {
+        return error;
+      }
+    }
   }
 }
